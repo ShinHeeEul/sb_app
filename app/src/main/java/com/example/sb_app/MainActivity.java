b82ac215d581ea975099f4b2bf24af70cfffd8c1
ShinHeeEul <sheshe7015@gmail.com>
2021-09-01 21:12:37 +0900

    example

--- app/src/main/java/com/example/sb_app/MainActivity.java
+++ app/src/main/java/com/example/sb_app/MainActivity.java
@@ -13,8 +13,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        start();
+
         setContentView(R.layout.activity_main);
+        start();
     }
 
     private void start() {
