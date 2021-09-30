1fe458d0d95da98096604793458486aa27e3150f
ShinHeeEul <sheshe7015@gmail.com>
2021-09-30 23:42:55 +0900

    example

--- app/src/main/java/com/example/sb_app/MainActivity.java
+++ app/src/main/java/com/example/sb_app/MainActivity.java
@@ -72,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
 
         // xml에 정의한 이미지뷰 찾고
         TouchImageView mImageView = (TouchImageView) findViewById(R.id.subway);
+        Log.d("hello","hello");
 
 
 
